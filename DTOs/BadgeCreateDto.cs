392279c5af471afda7e386798fb8f9e81a254998
Chetan Patil <crp13749@gmail.com>
2026-02-20 18:33:12 +0530

    added badge tools as well as csv file tool

---
+++ DTOs/BadgeCreateDto.cs
@@ -0,0 +1,15 @@
+﻿namespace BadgeCraft_Net.DTOs
+{
+    public class BadgeCreateDto
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string BgColor { get; set; }
+        public string TextColor { get; set; }
+        
+        public int OrganizationId { get; set; }
+
+        public IFormFile Logo { get; set; }
+       
+    }
+}
--- obj/Debug/net8.0/BadgeCraft_Net.AssemblyInfo.cs
+++ obj/Debug/net8.0/BadgeCraft_Net.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BadgeCraft_Net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ac53e77de978027619b949a8994b14299339fb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b2a3d756ab2243d01fb88c7a63e26395f36cf7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BadgeCraft_Net")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BadgeCraft_Net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
